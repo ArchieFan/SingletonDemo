59f1e0b06fc90dce175737b910103f1fa2e148df
Archie Fan <archiefan@gmail.com>
2022-05-24 23:06:26 +0100

    Add project files.

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintMsg("From Employee");
+
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintMsg("From Student");
+
+            Console.WriteLine();
+
+            SingletonMethod1 fromM1 = SingletonMethod1.GetInstance();
+            fromM1.PrintMsg("From Method 1");
+
+            SingletonMethod1 fromM1again = SingletonMethod1.GetInstance();
+            fromM1again.PrintMsg("From Method 1 again");
+
+            Console.WriteLine();
+
+            SingletonMethod2 fromM2 = SingletonMethod2.instance;
+            fromM2.PrintMsg("From Method 2");
+
+            SingletonMethod2 fromM2again = SingletonMethod2.instance;
+            fromM2again.PrintMsg("From Method 2 again");
+
+            Console.WriteLine();
+
+            SingletonMethod3 fromM3= SingletonMethod3.instance;
+            fromM2.PrintMsg("From Method 3");
+
+            SingletonMethod3 fromM3again = SingletonMethod3.instance;
+            fromM2again.PrintMsg("From Method 3 again");
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonDemo/Singleton.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    // Using Method
+    class SingletonMethod1
+    {
+        private SingletonMethod1() {
+            Console.WriteLine($" SingletonMethod1 ");
+        }
+
+        private static SingletonMethod1 _instance;
+        public static SingletonMethod1 GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new SingletonMethod1();
+            }
+            return _instance;
+        }
+
+        public void PrintMsg(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    // No Thread Safe Singleton - Using Properties
+    public sealed class SingletonMethod2
+    {
+        private SingletonMethod2() {
+            Console.WriteLine($" SingletonMethod2 ");
+        }
+
+        private static SingletonMethod2 _instance = null;
+
+        public static SingletonMethod2 instance
+        {
+            get {
+                if (_instance == null)
+                {
+                    _instance = new SingletonMethod2();
+                }
+                return _instance;
+            }
+
+        }
+
+        public void PrintMsg(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    // No Thread Safe Singleton - Using Properties (2)
+    internal sealed class Singleton
+    {
+        private static int counter = 0;
+        
+        private static Singleton instance = null;
+        public static Singleton GetInstance {
+            get {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine($"Counter value: {counter} ");
+        }
+        public void PrintMsg(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    // Using lock
+    public sealed class SingletonMethod3
+    {
+        private static readonly object Instancelock = new object();
+        private SingletonMethod3() {
+            Console.WriteLine($" SingletonMethod3 ");
+        }
+
+        private static SingletonMethod3 _instance = null;
+
+        public static SingletonMethod3 instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (Instancelock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new SingletonMethod3();
+                        }
+                    }
+                }
+                return _instance;
+            }
+
+        }
+
+        public void PrintMsg(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
